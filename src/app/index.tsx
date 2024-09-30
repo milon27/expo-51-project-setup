d4dc6c5ca47834bf03a3f0b6a2f1066c833b66f2
milon27 <mdjahidulislammilon@gmail.com>
2024-09-30 21:37:41 +0600

    native wind added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Home() {
   return (
     <View>
-      <Text>index</Text>
+      <Text className="text-4xl text-red-500">index</Text>
     </View>
   );
 }
